7133ae8020f31e5733d4f28829579c2b676ef278
Gitting better <StevenM82@users.noreply.github.com>
2021-12-27 15:15:36 -0800

    default title works for all pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Checking the default title',
+    title: 'Wanderlust a la Carte',
     htmlAttrs: {
       lang: 'en'
     },
