62e6e519afb145d861b3d61321f75c8a0f0c8bfd
Gitting better <StevenM82@users.noreply.github.com>
2021-12-27 15:36:01 -0800

    information doesn't refreshon reload after saving

--- pages/Boot-grid.vue
+++ pages/Boot-grid.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <h1>{{ title }}</h1>
     <div class="row text-center">
       <h1 class="col-12 display-3">Wanderlust a la Carte</h1>
       <h2 class="col-12 display-4">Not lost, but finding</h2>
@@ -32,14 +31,8 @@
     name: 'Boot-grid',
     layout: 'default',
 
-    data() {
-      return {
-        title: 'Boot-grid'
-      }
-    },
-
     head: {
-      title: 'Boot-grid',
+      title: 'Boot-grid of the beginning',
       meta:[
         {
           hid: 'description',
