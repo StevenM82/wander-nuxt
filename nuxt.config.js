66295b82693a3811d266a9acdcf0a33cf11d685a
Gitting better <StevenM82@users.noreply.github.com>
2021-12-27 15:26:10 -0800

    the default maybe working now

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { 
         hid: 'description', 
-        // name: 'description', 
+        name: 'Wandering', 
         content: 'Van traveling both local and abroad', 
       }
     ],
