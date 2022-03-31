ffdacad8f21bc44356db5d977055b503686151e4
Gitting better <StevenM82@users.noreply.github.com>
2022-03-30 22:40:24 -0700

    working on styling the index page below 768px

--- components/IndexComponent.vue
+++ components/IndexComponent.vue
@@ -1,23 +1,23 @@
 <template>
-    <p></p>
+  <p></p>
 </template>
 
 <script>
 export default {
-    name: 'IndexComponent',
-    data() {
-        return {
-            props: {
-                id: {
-                    type: Number,
-                    required: true
-                },
-                text: {
-                    type: String,
-                    required: true
-                }
-            }
-        }
-    }
-}
+  name: 'IndexComponent',
+  data() {
+    return {
+      props: {
+        id: {
+          type: Number,
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+      },
+    };
+  },
+};
 </script>
